e37e7fab8ebd86cb965b4523f34e9d6b5042a200
Hiero-o <gustavoardss@gmail.com>
2025-06-10 22:59:05 -0300

    exercicio

---
+++ exercicios/EX03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void main () {
+
+    int d, e, f, r;
+    
+
+
+    printf("escreva 3 numeros: \n");
+
+
+    scanf("%d %d %d",&d,&e,&f);
+
+    r = d*e*f;
+
+    printf("\n O resultado das multiplicacoes foi %d", r);
+
+    
+    system("pause");
+
+}
