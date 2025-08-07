6fc857e364f985d7f1c406d39be78cfc317e9103
Hiero-o <gustavoardss@gmail.com>
2025-08-07 20:22:05 -0300

    terminei o basico do C
    finalmente? sim.

--- matriz-exe/exercicios/inverteM.c
+++ matriz-exe/exercicios/inverteM.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define LINHA 2
+#define COLUNA 2
 
 
 
 int main () {
 
-    printf("Escreva quatro numeros e em seguida, ele te mostrará a ordem invertida \n");
+    printf("Escreva %d numeros e em seguida, ele vai te mostrar a ordem invertida \n", LINHA * COLUNA);
 
 
-    int matriz [2] [2];
+    int matriz [LINHA] [COLUNA];
+
+    
+
 
     int a,b;
+    int c,d =0;
+    for (a = 0; a < LINHA; a++){
 
-    for (a = 0; a < 2; a++){
-
-        for (b = 0; b < 2; b++){
+        for (b = 0; b < COLUNA; b++){
+            printf("Digite um valor para Matriz [%d], [%d] \n", a,b);
             scanf("%d", &matriz[a][b]);
-
+            
+    
         }
         printf("\n");
 
     }
 
-    for (a = 0; a < 2; a++){
+    for (a = LINHA - 1; a >= 0; a--){
 
-        for(b = 0; b < 2; b++){
+        for(b = COLUNA - 1; b >= 0; b--){
             printf("%d ", matriz[a][b]);
         }
         printf("\n");
@@ -33,10 +39,24 @@ int main () {
     }
 
 
+    /* OU
+    
+        usando matrizes auxiliares.
+        aux1, aux2
+
+        int aux1, aux2;
+
+        aux1 = matriz [0] [0]
+        aux2 = matriz [0] [1]
+
+        matriz [0][0] = matriz [1][0];
+        matriz [0][1] = matriz [1][1];
+        matriz [0][0] = aux1;
+        matriz [0][1] = aux2;
+    
+    */
+
         return 0;
-
-        
-
 }
 
 
